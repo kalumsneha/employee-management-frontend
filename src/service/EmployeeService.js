1cfd390b78af46a46acec45ee354cd35d2f3c993
mikalumfernando <kalumsneha@yahoo.com>
2024-11-05 19:04:41 +0100

    Initial Commit

---
+++ src/service/EmployeeService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_URL = "http://3.12.104.148:8080/employee-management/api/v1/employee";
+class EmployeeService {
+  //**Method to get all employee from our api or database */
+  getAllEmployee() {
+    return axios.get(BASE_URL);
+  }
+  /**MEthod to save employee */
+  saveEmployee(employeeData) {
+    return axios.post(BASE_URL, employeeData);
+  }
+  updateEmployee(id, employeeData) {
+    return axios.put(`${BASE_URL}/${id}`, employeeData);
+  }
+  getEmployeeById(id) {
+    return axios.get(`${BASE_URL}/${id}`);
+  }
+  deleteEmployee(id) {
+    return axios.delete(BASE_URL + "/" + id);
+  }
+}
+export default new EmployeeService();
